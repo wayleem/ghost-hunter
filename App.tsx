0317a894a2bb228df94627ca13d166abe921236a
way_leem <wayleemh@gmail.com>
2022-12-12 15:55:50 -0500

    refactor(app): remove persistor.

--- App.tsx
+++ App.tsx
@@ -1,17 +1,13 @@
 import { Provider } from 'react-redux'
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
 import { store } from './store'
 import Main from './src/Main'
 
-let persistor = persistStore(store)
-
 export default function App() {
-	return (
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<Main />
-			</PersistGate>
-		</Provider>
-	)
+  return (
+    <Provider store={store}>
+
+      <Main />
+
+    </Provider>
+  )
 }
