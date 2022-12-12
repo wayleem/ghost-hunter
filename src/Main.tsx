68235830047ef7727db1b59ee5eb35f3102dc54a
way_leem <wayleemh@gmail.com>
2022-12-12 17:32:17 -0500

    amend(main): add navigation to main.

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,9 +2,15 @@ import { StatusBar } from 'expo-status-bar'
 import { useSelector, useDispatch } from 'react-redux'
 import { cameraStatus } from '../types'
 import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Camera, CameraType, FlashMode, PermissionStatus } from 'expo-camera'
 import * as action from '../actions'
 import CameraPreview from './CameraPreview'
+import CameraView from './CameraView'
+import Start from './Start'
+import PhotoCollection from './PhotoCollection'
+import { Navigate } from 'react-router'
 
 let camera: Camera
 
@@ -19,21 +25,20 @@ const styles = StyleSheet.create({
 
 export default function Main() {
 	const startCamera = useSelector((state: cameraStatus) => state.startCamera)
-	const previewVisible = useSelector(
-		(state: cameraStatus) => state.previewVisible
-	)
-	const capturedImage = useSelector(
-		(state: cameraStatus) => state.capturedImage
-	)
+	const previewVisible = useSelector((state: cameraStatus) => state.previewVisible)
+	const capturedImage = useSelector((state: cameraStatus) => state.capturedImage)
 	const cameraType = useSelector((state: cameraStatus) => state.cameraType)
 	const flashMode = useSelector((state: cameraStatus) => state.flashMode)
-	const collection = useSelector((state: cameraStatus) => state.collection)
+	const collectionVisible = useSelector((state: cameraStatus) => state.collectionVisible)
+	const detection = useSelector((state: cameraStatus) => state.detection)
 	const dispatch = useDispatch()
 
 	const __startCamera = async () => {
 		const { status } = await Camera.requestCameraPermissionsAsync()
 		if (status === PermissionStatus.GRANTED) {
 			dispatch(action.setStartCamera(true))
+			Navigate({ to: "CameraView" })
+			console.log("click")
 		} else {
 			Alert.alert('Access denied')
 		}
@@ -42,11 +47,14 @@ export default function Main() {
 	const __takePicture = async () => {
 		const photo: any = await camera.takePictureAsync()
 		dispatch(action.setPreviewVisible(true))
+		Navigate({ to: "CameraView" })
 		dispatch(action.setCapturedImage(photo))
 	}
 
 	const __savePhoto = () => {
 		dispatch(action.addPhoto(capturedImage))
+		dispatch(action.setCapturedImage(""))
+		dispatch(action.setPreviewVisible(false))
 		__startCamera()
 	}
 
@@ -74,25 +82,57 @@ export default function Main() {
 		}
 	}
 
-	const __openCollection = () => { }
+	const __openCollection = () => {
+		dispatch(action.setCollectionVisible(true))
+	}
+
+	const __handleDetection = () => {
+		while (startCamera) {
+
+		}
+	}
+
+	const Stack = createNativeStackNavigator()
 
 	return (
-		<View style={styles.container}>
-			{startCamera ? (
-				<View
-					style={{
-						flex: 1,
-						width: '100%',
-					}}
-				>
-					{previewVisible && capturedImage ? (
-						<CameraPreview
-							photo={capturedImage}
-							savePhoto={__savePhoto}
-							retakePicture={__retakePicture}
-						/>
-					) : (
-						<Camera
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Start">
+				<Stack.Screen name="Start" component={Start} initialParams={{ startCamera: __startCamera }} />
+				<Stack.Screen name="CameraView" component={CameraView} initialParams={{
+					cameraType: __startCamera,
+					flashMode: flashMode,
+					handleFlashMode: __handleFlashMode,
+					takePicture: __takePicture,
+					switchCamera: __switchCamera,
+					openCollection: __openCollection,
+					detection: detection
+				}} />
+				<Stack.Screen name="CameraPreview" component={CameraPreview} initialParams={{
+					photo: capturedImage,
+					retakePicture: __retakePicture,
+					savePhoto: __savePhoto
+				}} />
+				<Stack.Screen name="PhotoCollection" component={PhotoCollection} initialParams={{ startCamera: __startCamera }} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
+	/*
+		return (
+			<View style={styles.container}>
+				{startCamera ? (
+					<View
+						style={{
+							flex: 1,
+							width: '100%',
+						}}
+					>
+						{previewVisible && capturedImage ? (
+							<CameraPreview
+								photo={capturedImage}
+								savePhoto={__savePhoto}
+								retakePicture={__retakePicture}
+							/>
+						) : (<Camera
 							type={cameraType}
 							flashMode={flashMode}
 							style={{ flex: 1 }}
@@ -100,127 +140,22 @@ export default function Main() {
 								camera = r
 							}}
 						>
-							<View
-								style={{
-									flex: 1,
-									width: '100%',
-									backgroundColor: 'transparent',
-									flexDirection: 'row',
-								}}
-							>
-								<View
-									style={{
-										position: 'absolute',
-										left: '5%',
-										top: '10%',
-										flexDirection: 'column',
-										justifyContent: 'space-between',
-									}}
-								>
-									<TouchableOpacity
-										onPress={__handleFlashMode}
-										style={{
-											backgroundColor: flashMode === 'off' ? '#000' : '#fff',
-											borderRadius: 50,
-											height: 25,
-											width: 25,
-										}}
-									>
-										<Text
-											style={{
-												fontSize: 20,
-											}}
-										>
-											⚡️
-										</Text>
-									</TouchableOpacity>
-									<TouchableOpacity
-										onPress={__switchCamera}
-										style={{
-											marginTop: 20,
-											borderRadius: 50,
-											height: 25,
-											width: 25,
-										}}
-									>
-										<Text
-											style={{
-												fontSize: 20,
-											}}
-										>
-											{cameraType === 'front' ? '🤳' : '📷'}
-										</Text>
-									</TouchableOpacity>
-								</View>
-								<View
-									style={{
-										position: 'absolute',
-										bottom: 0,
-										flexDirection: 'row',
-										flex: 1,
-										width: '100%',
-										padding: 20,
-										justifyContent: 'space-between',
-									}}
-								>
-									<View
-										style={{
-											alignSelf: 'center',
-											flex: 1,
-											alignItems: 'center',
-										}}
-									>
-										<TouchableOpacity
-											onPress={__takePicture}
-											style={{
-												width: 70,
-												height: 70,
-												bottom: 0,
-												borderRadius: 50,
-												backgroundColor: '#fff',
-											}}
-										/>
-									</View>
-								</View>
-							</View>
+							<CameraView
+								cameraType={cameraType}
+								flashMode={flashMode}
+								handleFlashMode={__handleFlashMode}
+								takePicture={__takePicture}
+								switchCamera={__switchCamera}
+								openCollection={__openCollection}
+								detection={detection}
+							/>
 						</Camera>
-					)}
-				</View>
-			) : (
-				<View
-					style={{
-						flex: 1,
-						backgroundColor: '#fff',
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}
-				>
-					<TouchableOpacity
-						onPress={__startCamera}
-						style={{
-							width: 130,
-							borderRadius: 4,
-							backgroundColor: '#14274e',
-							flexDirection: 'row',
-							justifyContent: 'center',
-							alignItems: 'center',
-							height: 40,
-						}}
-					>
-						<Text
-							style={{
-								color: '#fff',
-								fontWeight: 'bold',
-								textAlign: 'center',
-							}}
-						>
-							Take picture
-						</Text>
-					</TouchableOpacity>
-				</View>
-			)}
-
-			<StatusBar style="auto" />
-		</View>
-	)
+						)}
+	
+					</View>
+				) : (
+					<Start startCamera={__startCamera} />
+				)}
+			</View>
+		)*/
 }
