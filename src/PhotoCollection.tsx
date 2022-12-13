14e46c26c2271c34a31b545ef782799e0c9341be
way_leem <wayleemh@gmail.com>
2022-12-13 03:27:06 -0500

    init(PhotoCollection): add photo collection component.

--- src/PhotoCollection.tsx
+++ src/PhotoCollection.tsx
@@ -1,6 +1,22 @@
-import { Text, View, TouchableOpacity, ImageBackground } from 'react-native'
+import { ScrollView, Text, View, TouchableOpacity, ImageBackground, Image, FlatList, ListRenderItem } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
+import { setCollectionVisible } from '../actions'
+import { cameraStatus } from '../types'
 
 export default function PhotoCollection() {
+    const collection = useSelector((state: cameraStatus) => state.collection)
+    /*const Item = ({ data }: { data: any }) => (
+        <View
+            style={{
+                borderRadius: 10,
+                padding: 20,
+                marginVertical: 8,
+                marginHorizontal: 16,
+            }}>
+            <Image source={{ uri: data && data.uri }} />
+        </View>
+    )
+    const renderItem: ListRenderItem<any> = ({ item }) => <Item data={item.uri} />
     return (
         <View
             style={{
@@ -9,6 +25,41 @@ export default function PhotoCollection() {
                 width: '100%',
                 height: '100%',
             }}
-        ></View>
+        >
+            <FlatList
+                data={collection}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => { return index.toString() }}
+            />
+        </View>
+    )
+}*/
+    console.log("photocollection: " + collection)
+    return (
+        <View
+            style={{
+                backgroundColor: '#fff',
+                flex: 1,
+                width: '100%',
+                height: '100%',
+            }}
+        >
+            <ScrollView style={{
+                marginHorizontal: 20,
+            }}>
+                {collection.map((image) => {
+                    return (
+                        <View key={image}>
+                            <Image style={{
+                                width: '100%',
+                                aspectRatio: 1
+
+                            }}
+                                source={{ uri: image }} />
+                        </View>
+                    )
+                })}
+            </ScrollView>
+        </View>
     )
 }
